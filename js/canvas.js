424382930fcc5303449022523f615bcb2e8e4a8d
Timo <30553356+y21@users.noreply.github.com>
2018-06-21 18:11:24 +0200

    Moved canvas part into canvas.js

---
+++ js/canvas.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth / 1.25;
+canvas.height = window.innerHeight / 1.25;
+ctx.font = "20px arial";
+ctx.fillStyle = "white";
