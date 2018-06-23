103acf53da47fdfce3d7c84b0b664a5ba0628442
Timo <30553356+y21@users.noreply.github.com>
2018-06-23 19:45:44 +0200

    Emit on register-btn and listen to register event

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,9 +1,18 @@
-const socket = io.connect("localhost");
+const socket = io.connect("blobs-io.glitch.me");
 
-socket.emit("register", {
-  
+document.getElementById("register-btn").addEventListener("click", function() {
+  socket.emit("register", {
+    username: document.getElementById("user").value,
+    password: document.getElementById("pass").value
+  });
 });
 
+const message = "<div id=\"<type>-notif\"><message></div>";
+
 socket.on("register", function(data){
-  
+  if([400, 500].indexOf(data.status) > -1){
+    document.getElementById("auth").innerHTML = message.replace("<type>", "failure").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
+  } else {
+  document.getElementById("auth").innerHTML = message.replace("<type>", "success").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
+  }
 });
