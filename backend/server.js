c1bf8ce5c30bcb74767b48f837ff868ce5c15528
Timo <30553356+y21@users.noreply.github.com>
2018-06-23 19:00:28 +0200

    Registration part (backend)

--- backend/server.js
+++ backend/server.js
@@ -2,30 +2,56 @@ const express = require("express");
 const app = express();
 const socket = require("socket.io");
 const server = app.listen(process.env.PORT, () => {
-	console.log("App started");
+    console.log("App started");
 });
+const bcrypt = require("bcrypt");
+const sqlite = require("sqlite");
 app.use(express.static("public"));
-
+sqlite.open("db.sqlite");
 const io = socket(server);
 
+function displayError(msg, data, event, status) {
+    io.to(data.id).emit(event, {
+        status: status,
+        message: msg
+    });
+}
+
 io.on("connection", data => {
-	data.on("register", res => {
-		
-		// If username/password is undefined
-		if(!res.username || !res.password) return io.to(data.id).emit("register", {
-			status: 400,
-			message: "Either username or password is undefined."
-		});
-		
-		// Username/Password length check
-		if(res.username.length < 3 || res.username.length > 10) return io.to(data.id).emit("register", {
-			status: 400,
-			message: "Username needs to be at least 3 characters long and must not be longer than 10 characters."
-		});
-		
-		if(res.password.length < 5 || res.password.length > 32) return io.to(data.id).emit("register", {
-			status: 400,
-			message: "Password needs to be at least 5 characters long and must not be longer than 32 characters."
-		});
-	});
+    data.on("register", res => {
+        // If username/password is undefined
+        if (!res.username || !res.password) return io.to(data.id).emit("register", {
+            status: 400,
+            message: "Either username or password is undefined."
+        });
+
+        // Username/Password length check
+        if (res.username.length < 3 || res.username.length > 10) return io.to(data.id).emit("register", {
+            status: 400,
+            message: "Username needs to be at least 3 characters long and must not be longer than 10 characters."
+        });
+
+        if (res.password.length < 5 || res.password.length > 32) return io.to(data.id).emit("register", {
+            status: 400,
+            message: "Password needs to be at least 5 characters long and must not be longer than 32 characters."
+        });
+        let hash = bcrypt.hashSync(res.password, 10);
+
+        sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
+            prepare.get([res.username]).then(result => {
+                if (result) return displayError("Username is already taken.", data, "register", 400);
+                sqlite.prepare("INSERT INTO accounts VALUES (?, ?, 0)").then(prepare2 => {
+                    prepare2.run([res.username, hash]).then(() => {
+                        io.to(data.id).emit("register", {
+                            status: 200,
+                            message: "Account successfully created!"
+                        });
+                    }).catch(console.log);
+                }).catch(console.log);
+            });
+        }).catch(err => {
+            if (err.toString().includes("no such table: accounts")) return displayError("A problem occured on the server-side.", data, "register", 500);
+        });
+
+    });
 });
