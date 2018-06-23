1d40f821cbe6274f3af64f0013aa7f3f93d168db
Timo <30553356+y21@users.noreply.github.com>
2018-06-23 17:23:32 +0200

    Check if username > 3 && username < 10 and pw...

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,22 @@ const io = socket(server);
 
 io.on("connection", data => {
 	data.on("register", res => {
+		
 		// If username/password is undefined
 		if(!res.username || !res.password) return io.to(data.id).emit("register", {
 			status: 400,
 			message: "Either username or password is undefined."
+		});
+		
+		// Username/Password length check
+		if(res.username.length < 3 || res.username.length > 10) return io.to(data.id).emit("register", {
+			status: 400,
+			message: "Username needs to be at least 3 characters long and must not be longer than 10 characters."
+		});
+		
+		if(res.password.length < 5 || res.password.length > 32) return io.to(data.id).emit("register", {
+			status: 400,
+			message: "Password needs to be at least 5 characters long and must not be longer than 32 characters."
 		});
 	});
 });
