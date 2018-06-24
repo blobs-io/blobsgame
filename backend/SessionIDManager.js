0b18c50632ae944114577548558324649bab0994
Timo <30553356+y21@users.noreply.github.com>
2018-06-24 20:03:57 +0200

    Add sessionidmanager and assign session def. value
    ... and set exired to Date.now + 9e5 which is in 15 minutes

---
+++ backend/SessionIDManager.js
@@ -0,0 +1,19 @@
+const { randomBytes } = require("crypto");
+/**
+ * 
+ * @param {object} database The database object (must have a run method - recommended sql client: sqlite)
+ * @param {string} username The username
+ * @param {string=} session The session id (recommended length: 16)
+ * @param {object=|number=} expires Either a timestamp or a date object when the session id expires (default: 15 minutes)
+ * @returns {promise<string>} The registered 
+ */
+exports.registerID = (database, username, session, expires) => {
+    // Assign random value to session if undefined
+    if(typeof session === "undefined") {
+        session = "";
+        for(let i = 0; i < 8; ++i){
+            session += (crypto.randomBytes(1).readUInt8() & 0xFF).toString(36);
+        }
+    }
+    if(typeof expired === "undefined") expired = Date.now() + 9e5;
+}
