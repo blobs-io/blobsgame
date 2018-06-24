628a9ca8d1991a9870b0d5fbaa1aa3a6cdf13887
Timo <30553356+y21@users.noreply.github.com>
2018-06-24 17:03:30 +0200

    Check window.location.href before listen/emit...

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,18 +1,30 @@
-const socket = io.connect("blobs-io.glitch.me");
+const socket = io.connect("localhost:3000");
 
-document.getElementById("register-btn").addEventListener("click", function() {
-  socket.emit("register", {
-    username: document.getElementById("user").value,
-    password: document.getElementById("pass").value
-  });
-});
 
-const message = "<div id=\"<type>-notif\"><message></div>";
+if (/register(\/.*)?$/.test(window.location.href)) {
+    document.getElementById("register-btn").addEventListener("click", function () {
+        socket.emit("register", {
+            username: document.getElementById("user").value,
+            password: document.getElementById("pass").value
+        });
+    });
 
-socket.on("register", function(data){
-  if([400, 500].indexOf(data.status) > -1){
-    document.getElementById("auth").innerHTML = message.replace("<type>", "failure").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
-  } else {
-  document.getElementById("auth").innerHTML = message.replace("<type>", "success").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
-  }
-});
+    const message = "<div id=\"<type>-notif\"><message></div>";
+    socket.on("register", function (data) {
+        if ([400, 500].indexOf(data.status) > -1) {
+            document.getElementById("auth").innerHTML = message.replace("<type>", "failure").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
+        } else {
+            document.getElementById("auth").innerHTML = message.replace("<type>", "success").replace("<message>", data.message) + document.getElementById("auth").innerHTML;
+        }
+    });
+} else if (/login(\/.*)?$/.test(window.location.href)) {
+    document.getElementById("login-btn").addEventListener("click", function (data) {
+        socket.emit("login", {
+            username: document.getElementById("user").value,
+            password: document.getElementById("pass").value
+        });
+        socket.on("login", function (data) {
+            console.log(data);
+        });
+    });
+}
