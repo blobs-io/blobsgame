041d034db91a3301c4552dd7877ee501e0a4939f
Timo <30553356+y21@users.noreply.github.com>
2018-06-24 17:02:28 +0200

    Handle login event (server-side) and fix spaces

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,14 @@ const server = app.listen(process.env.PORT, () => {
 });
 const bcrypt = require("bcrypt");
 const sqlite = require("sqlite");
-const { existsSync, writeFileSync } = require("fs");
+const {
+    existsSync,
+    writeFileSync
+} = require("fs");
 app.use(express.static("public"));
-if(!existsSync("./db.sqlite")) writeFileSync("db.sqlite", "");
-sqlite.open("db.sqlite").then(()=>{
-  sqlite.all("SELECT * FROM accounts").then(console.log);
-});
+if (!existsSync("./db.sqlite")) writeFileSync("./db.sqlite", "");
 const io = socket(server);
+sqlite.open("db.sqlite");
 
 /**
  * Displays an error by emitting to websocket on clientside
@@ -30,9 +31,33 @@ function displayError(msg, data, event, status) {
     });
 }
 
-
-
 io.on("connection", data => {
+    data.on("login", res => {
+        // If username/password is undefined
+        if (!res.username || !res.password) return io.to(data.id).emit("register", {
+            status: 400,
+            message: "Either username or password is undefined."
+        });
+        sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
+            prepare.get([res.username]).then(result => {
+                if(bcrypt.compareSync(res.password, result.password)){
+                    io.to(data.id).emit("login", {
+                        status: 200,
+                        message: "Successfully logged in."
+                    });
+                } else {
+                    displayError("Incorrect username or password.", data, "login", 400);
+                }
+            });
+        }).catch(err => {
+            console.log(err)
+            if (err.toString().includes("no such table: accounts")) {
+                displayError("A problem occured on the server-side.", data, "register", 500);
+                sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").catch(console.log);
+            }
+        });
+    });
+
     data.on("register", res => {
         // If username/password is undefined
         if (!res.username || !res.password) return io.to(data.id).emit("register", {
@@ -50,29 +75,29 @@ io.on("connection", data => {
             status: 400,
             message: "Password needs to be at least 5 characters long and must not be longer than 32 characters."
         });
-      
-        if(/[^\w ]+/.test(res.username)) return displayError("Username should only contain A-Za-z_ ", data, "register", 400);
-      
+
+        if (/[^\w ]+/.test(res.username)) return displayError("Username should only contain A-Za-z_ ", data, "register", 400);
+
         const hash = bcrypt.hashSync(res.password, 10);
-        
-       sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
-         prepare.get([ res.username ]).then(result => {
-           if(result) return displayError("Username is already taken.", data, "register", 400);
-           sqlite.prepare("INSERT INTO accounts VALUES (?, ?, 0)").then(prepare2 => {
-             prepare2.run([ res.username, hash ]).then(() => {
-               io.to(data.id).emit("register", {
-                 status: 200,
-                 message: "Account successfully created!"
-               });
-             }).catch(console.log);
-           }).catch(console.log);
-         });
-       }).catch(err => {
-         if(err.toString().includes("no such table: accounts")) {
-           displayError("A problem occured on the server-side.", data, "register", 500);
-           sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").catch(console.log);
-         }
-       });
-        
+
+        sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
+            prepare.get([res.username]).then(result => {
+                if (result) return displayError("Username is already taken.", data, "register", 400);
+                sqlite.prepare("INSERT INTO accounts VALUES (?, ?, 0)").then(prepare2 => {
+                    prepare2.run([res.username, hash]).then(() => {
+                        io.to(data.id).emit("register", {
+                            status: 200,
+                            message: "Account successfully created!"
+                        });
+                    }).catch(console.log);
+                }).catch(console.log);
+            });
+        }).catch(err => {
+            if (err.toString().includes("no such table: accounts")) {
+                displayError("A problem occured on the server-side.", data, "register", 500);
+                sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").catch(console.log);
+            }
+        });
+
     });
 });
