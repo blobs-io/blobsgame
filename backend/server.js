415cfffb60823abc9dafd9145796023b0c1ef4fa
Timo <30553356+y21@users.noreply.github.com>
2018-06-27 00:26:07 +0200

    Create tables if they don't exist and handle getCaptcha

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,22 @@ const {
 app.use(express.static("public"));
 if (!existsSync("./db.sqlite")) writeFileSync("./db.sqlite", "");
 const io = socket(server);
-sqlite.open("db.sqlite");
+sqlite.open("db.sqlite").then(() => {
+    sqlite.get("SELECT * FROM accounts").catch(err => {
+        if (err.toString().includes("no such table: accounts")) {
+            sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").then(() => {
+                console.log(`[${new Date().toLocaleString()}] Created table: accounts.`);
+            }).catch(console.log);
+        } else console.log(err);
+    });
+    sqlite.get("SELECT * FROM sessionids").catch(err => {
+        if (err.toString().includes("no such table: sessionids")) {
+            sqlite.run("CREATE TABLE sessionids (`username` TEXT, `sessionid` TEXT, `expires` TEXT)").then(() => {
+                console.log(`[${new Date().toLocaleString()}] Created table: sessionids.`);
+            }).catch(console.log);
+        } else console.log(err);
+    });
+}).catch(console.log);
 const sessions = require("./SessionIDManager");
 let captchas = new Array();
 
@@ -40,7 +55,7 @@ setInterval(() => {
 io.on("connection", data => {
     // Event listeners
     data.on("appCreate", _ => require("./events/appCreate").run(_, displayError, sessions, io, data, sqlite));
-    data.on("getCaptcha", () => require("./events/getCaptcha").run(sessions, io, data, captchas));
+    data.on("getCaptcha", () => require("./events/getCaptcha").run(sessions, io, data, captchas).then(res => captchas = res));
     data.on("login", res => require("./events/login").run(res, io, data, sqlite, bcrypt, sessions, displayError));
     data.on("register", res => require("./events/register").run(res, io, data, displayError, captchas, bcrypt, sqlite));
 });
