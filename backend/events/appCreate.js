2393160253fd5f4328ca6fe3a36e2275e142cafa
Timo <30553356+y21@users.noreply.github.com>
2018-06-26 23:55:57 +0200

    Move appCreate event to /events/appCreate.js

---
+++ backend/events/appCreate.js
@@ -0,0 +1,31 @@
+class appCreateEvent {};
+/**
+ * Runs the appCreate Event
+ * 
+ * @param {Array} args Method arguments
+ * @returns {Promise<bool>} Whether an OK http code was emitted (200) or a bad one (4xx/5xx)
+ */
+appCreateEvent.run = (...args) => {
+    const [sessionid, displayError, sessions, io, data, sqlite] = args;
+    return new Promise(resolve => {
+        if (!sessionid) return displayError("No session ID provided.", data, "appCreate", 400);
+        sessions.getSession(sqlite, {
+            type: "session",
+            value: sessionid
+        }).then(session => {
+            if (!session) {
+                resolve(false);
+                return displayError("Session ID not found", data, "appCreate", 401);
+            } else {
+                resolve(true);
+                io.to(data.id).emit("appCreate", {
+                    status: 200,
+                    username: session.username,
+                    expiresAt: session.expires
+                });
+            }
+        });
+    });
+};
+
+module.exports = appCreateEvent;
