8447ef364436254fd7afb96929396b07c3d5d50e
Timo <30553356+y21@users.noreply.github.com>
2018-06-27 20:07:37 +0200

    Add getBRFromPlayer utility method

---
+++ backend/utils/getBRFromPlayer.js
@@ -0,0 +1,10 @@
+module.exports = (username, sqlite) => {
+    return new Promise((resolve, reject) => {
+        sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
+            prepare.get([username]).then(result => {
+                if(!result) reject(`Player with username "${username}" does not exist.`);
+                resolve(result.br);
+            })
+        }).catch(reject);
+    });
+}
