7494341bba0b53da7068d1d3e4e33ed311de21d7
Timo <30553356+y21@users.noreply.github.com>
2018-06-27 17:29:32 +0200

    displayError requires io object

--- backend/events/register.js
+++ backend/events/register.js
@@ -25,15 +25,15 @@ registerEvent.run = (...args) => {
             message: "Password needs to be at least 5 characters long and must not be longer than 32 characters."
         });
 
-        if (/[^\w ]+/.test(res.username)) return displayError("Username should only contain A-Za-z_ ", data, "register", 400);
+        if (/[^\w ]+/.test(res.username)) return displayError("Username should only contain A-Za-z_ ", data, "register", 400, io);
 
-        if (!captchas.find(val => val.captcha === res.captcha)) return displayError("Captcha is not correct", data, "register", 400);
+        if (!captchas.find(val => val.captcha === res.captcha)) return displayError("Captcha is not correct", data, "register", 400, io);
 
         const hash = bcrypt.hashSync(res.password, 10);
 
         sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
             prepare.get([res.username]).then(result => {
-                if (result) return displayError("Username is already taken.", data, "register", 400);
+                if (result) return displayError("Username is already taken.", data, "register", 400, io);
                 sqlite.prepare("INSERT INTO accounts VALUES (?, ?, 0)").then(prepare2 => {
                     prepare2.run([res.username, hash]).then(() => {
                         io.to(data.id).emit("register", {
@@ -50,7 +50,7 @@ registerEvent.run = (...args) => {
             });
         }).catch(err => {
             if (err.toString().includes("no such table: accounts")) {
-                displayError("A problem occured on the server-side.", data, "register", 500);
+                displayError("A problem occured on the server-side.", data, "register", 500, io);
                 sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").catch(console.log);
             }
         });
