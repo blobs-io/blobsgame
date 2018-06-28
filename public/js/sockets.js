2d783c15901abe98d9ebe3dd02895bd6a7560a55
Timo <30553356+y21@users.noreply.github.com>
2018-06-29 00:26:06 +0200

    Use JS to append all elements

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -46,9 +46,76 @@ if (/register(\/.*)?$/.test(window.location.href)) {
     if (sessionid.length > 0) {
         socket.emit("appCreate", sessionid[0].substr(sessionid[0].indexOf("=") + 1));
         socket.on("appCreate", function (data) {
-            if (data.status >= 400 && data.status < 500) document.location.href = server + "/login/";
-            document.getElementById("username").innerHTML = data.username;
-            document.getElementById("br").innerHTML = data.br;
+            if (data.status != 200){
+                document.location.href = server + "/login/";
+            }
+            const body = document.getElementsByTagName("body")[0]
+            body.removeChild(document.getElementsByTagName("iframe")[0]);
+            const elements = {
+                authDiv: document.createElement("div"),
+                brLabel: document.createElement("span"),
+                logoutBtn: document.createElement("span"),
+                greeting: document.createElement("h2"),
+                // Singleplayer Box
+                mmBoxesSP: document.createElement("div"),
+                spHeading: document.createElement("h3"),
+                blobKittenKnifeIMG: document.createElement("img"),
+                // Multiplayer box
+                mmBoxesMP: document.createElement("div"),
+                mpHeading: document.createElement("h3"),
+                blobEvilIMG: document.createElement("img"),
+                // Blobs box
+                mmBoxesBlobs: document.createElement("div"),
+                blobsHeading: document.createElement("h3"),
+                blobEyesIMG: document.createElement("img"),
+                // Settings box
+                mmBoxesSTG: document.createElement("div"),
+                stgHeading: document.createElement("h3"),
+                blobPeekIMG: document.createElement("img")
+            };
+
+            elements.authDiv.id = "auth";
+            elements.brLabel.className = "mm-br-label";
+            elements.brLabel.innerHTML = `<span id="br">${data.br}</span> BR`;
+            elements.logoutBtn.className = "mm-logout-btn";
+            elements.logoutBtn.innerHTML = "Log-out";
+            elements.greeting.innerHTML = `Welcome back, <span id="username">${data.username}</span>.`;
+            elements.mmBoxesSP.className = "mm-box mm-sp-box";
+            elements.spHeading.innerHTML = "Singleplayer";
+            elements.blobKittenKnifeIMG.src = "../assets/BlobKittenKnife.png";
+            elements.blobKittenKnifeIMG.id = "blob";
+            elements.mmBoxesMP.className = "mm-box mm-mp-box";
+            elements.mpHeading.innerHTML = "Multiplayer";
+            elements.blobEvilIMG.src = "../assets/blobevil.png";
+            elements.blobEvilIMG.id = "blob";
+            elements.mmBoxesBlobs.className = "mm-box mm-blobs-box"
+            elements.blobsHeading.innerHTML = "Blobs";
+            elements.blobEyesIMG.src = "../assets/blobeyes.png";
+            elements.blobEyesIMG.id = "blob";
+            elements.mmBoxesSTG.className = "mm-box mm-stg-box";
+            elements.stgHeading.innerHTML = "Settings";
+            elements.blobPeekIMG.src = "../assets/blobpeek.png";
+            elements.blobPeekIMG.id = "blob";
+            body.appendChild(elements.authDiv);
+            elements.authDiv.appendChild(elements.brLabel);
+            elements.authDiv.appendChild(elements.logoutBtn);
+            elements.authDiv.appendChild(elements.greeting);
+            // Appending singleplayer elements
+            elements.authDiv.appendChild(elements.mmBoxesSP);
+            elements.mmBoxesSP.appendChild(elements.spHeading);
+            elements.mmBoxesSP.appendChild(elements.blobKittenKnifeIMG);
+            // Appending multiplayer elements
+            elements.authDiv.appendChild(elements.mmBoxesMP);
+            elements.mmBoxesMP.appendChild(elements.mpHeading);
+            elements.mmBoxesMP.appendChild(elements.blobEvilIMG);
+            // Appending "blobs" elements
+            elements.authDiv.appendChild(elements.mmBoxesBlobs);
+            elements.mmBoxesBlobs.appendChild(elements.blobsHeading);
+            elements.mmBoxesBlobs.appendChild(elements.blobEyesIMG);
+            // Appending "settings" elements
+            elements.authDiv.appendChild(elements.mmBoxesSTG);
+            elements.mmBoxesSTG.appendChild(elements.stgHeading);
+            elements.mmBoxesSTG.appendChild(elements.blobPeekIMG);
         });
     } else {
         document.location.href = server + "/login/";
