b9fd5fa64963b46566c3d0043fe9d29450ee5c0d
y21 <artur.fiedler54@gmail.com>
2018-07-01 01:44:53 +0200

    Take sockets parameter in appCreateEvent

--- backend/events/appCreate.js
+++ backend/events/appCreate.js
@@ -6,18 +6,18 @@ class appCreateEvent {};
  * @returns {Promise<bool>} Whether an OK http code was emitted (200) or a bad one (4xx/5xx)
  */
 appCreateEvent.run = (...args) => {
-    const [sessionid, displayError, sessions, io, data, sqlite] = args;
+    const [sessionid, displayError, sessions, io, data, sqlite, sockets] = args;
     return new Promise((resolve, reject) => {
         if (!sessionid) return displayError("No session ID provided.", data, "appCreate", 400, io);
         sessions.getSession(sqlite, {
             type: "session",
             value: sessionid
         }).then(session => {
-            if (!session) {
+            if (!session && !sockets.some(_ => _.sessionid === sessionid)) {
                 resolve(false);
                 return displayError("Session ID not found", data, "appCreate", 401, io);
             } else {
-                if (Date.now() > session.expires) {
+                if (Date.now() > (session || Date.now() + 10000).expires) {
                     sessions.deleteSession(sqlite, {
                         type: "session",
                         value: sessionid
@@ -25,15 +25,23 @@ appCreateEvent.run = (...args) => {
                         displayError("Session expired", data, "appCreate", 403, io);
                     }).catch(console.log);
                 }
-                require("../utils/getBRFromPlayer")(session.username, sqlite).then(playerBR => {
-                    io.to(data.id).emit("appCreate", {
-                        status: 200,
-                        username: session.username,
-                        br: playerBR,
-                        expiresAt: session.expires
-                    });
-                    resolve(true);
-                }).catch(reject);
+                if(session) {
+                    require("../utils/getBRFromPlayer")(session.username, sqlite).then(playerBR => {
+                        io.to(data.id).emit("appCreate", {
+                            status: 200,
+                            username: session.username,
+                            br: playerBR,
+                            expiresAt: session.expires
+                        });
+                        sessions.deleteSession(sqlite, {
+                            type: "session",
+                            value: sessionid
+                        }).catch(reject);
+                        resolve(true);
+                    }).catch(reject);
+                } else {
+                    return displayError("Session expired", data, "appCreate", 403, io);
+                }
             }
         });
     });
