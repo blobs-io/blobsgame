79e57579df43b29c5d38da87e5532f5c82287a3b
y21 <artur.fiedler54@gmail.com>
2018-07-01 01:46:54 +0200

    Add disconnect and modify appCreate event

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ sqlite.open("db.sqlite").then(() => {
 const sessions = require("./SessionIDManager"),
 utils = { };
 let captchas = new Array(),
-sockets = new Map();
+sockets = new Array();
 
 require("./utils/utilManager")().then(utilities => {
     for(const val of utilities){
@@ -40,14 +40,26 @@ require("./utils/utilManager")().then(utilities => {
     }
 });
 
-
 setInterval(() => {
     captchas = captchas.filter(val => (val.createdAt + 18e4) > Date.now());
 }, 1000);
 
 io.on("connection", data => {
     // Event listeners
-    data.on("appCreate", _ => require("./events/appCreate").run(_, utils.displayError, sessions, io, data, sqlite));
+    data.on("disconnect", () => {
+        sockets = require("./events/disconnect").run(sockets, data);
+    });
+    data.on("appCreate", async _ => {
+        try {
+            require("./events/appCreate").run(_, utils.displayError, sessions, io, data, sqlite, sockets)
+            sockets.push({
+                sessionid: _, 
+                socketid: data.id
+            });
+        }catch(e){
+            console.log(e);
+        }     
+    });
     data.on("getCaptcha", () => require("./events/getCaptcha").run(sessions, io, data, captchas).then(res => captchas = res));
     data.on("login", res => require("./events/login").run(res, io, data, sqlite, bcrypt, sessions, utils.displayError));
     data.on("register", res => require("./events/register").run(res, io, data, utils.displayError, captchas, bcrypt, sqlite));
