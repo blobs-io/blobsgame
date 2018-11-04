4df0eaeec2d1e25e14c5528846a81bed54d0ac5e
Timo <30553356+y21@users.noreply.github.com>
2018-11-04 01:45:23 +0100

    Only take sessionid as param instead of entire blob obj

--- backend/events/ffaPlayerCreate.js
+++ backend/events/ffaPlayerCreate.js
@@ -1,19 +1,25 @@
 class ffaPlayerCreateEvent {};
 
-ffaPlayerCreateEvent.run = (...args) => {
-    const [blob, io, Base, data] = args;
-    if (typeof blob === "undefined") return;
-    if (blob === null) return;
-    if (blob.constructor.name !== "Object") return;
-    if (!blob.owner || !blob.br || !blob.x || !blob.y || !blob.img) return io.to(data.id).emit("ffaLoginFailed", "Properties must not have null as value.");
-    if (Base.gamemodes.ffa.players.find(v => v.owner === blob.owner)) return io.to(data.id).emit("ffaLoginFailed", "Username is already taken.");
-    if (typeof blob.x !== "number" || typeof blob.y !== "number") return;
-    blob.id = data.id;
-    blob.lastnom = Date.now();
-    blob._directionChange = Date.now();
+ffaPlayerCreateEvent.run = async (...args) => {
+    const [blob, io, Base, data, sockets] = args;
+    const socket = sockets.find(v => v.sessionid === blob);
+    if (!socket) return io.to(data.id).emit("ffaUnauthorized");
+    const nblob = {};
+    nblob.x = Math.floor(Math.random() * 600);
+    nblob.y = Math.floor(Math.random() * 600);
+    nblob.owner = socket.username;
+    nblob.direction = 0;
+    nblob.br = socket.br;
+    nblob.id = data.id;
+    nblob.lastnom = Date.now();
+    nblob._directionChange = Date.now();
 
-    Base.gamemodes.ffa.players.push(blob);
-    io.to(data.id).emit("ffaObjectsHeartbeat", Base.gamemodes.ffa.objects);
+    Base.gamemodes.ffa.players.push(nblob);
+    io.to(data).emit("ffaObjectsHeartbeat", Base.gamemodes.ffa.objects);
+    io.to(data.id).emit("ffaHeartbeat", {
+		username: socket.username,
+		br: socket.br
+	});
 };
 
 module.exports = ffaPlayerCreateEvent;
