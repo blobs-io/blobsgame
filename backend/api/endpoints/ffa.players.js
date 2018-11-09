5003088f5ae93f5fe9b12785a34a44c1cd486d1b
y21 <artur.fiedler54@gmail.com>
2018-11-09 14:07:32 +0100

    Use application/json as header

--- backend/api/endpoints/ffa.players.js
+++ backend/api/endpoints/ffa.players.js
@@ -3,7 +3,7 @@ const Base = require("../../Base");
 module.exports = class Players {
 	static run(...data) {
 		const [req, res] = data;
-		res.send(Base.gamemodes.ffa.players.map(v => Object.assign(v, {id: undefined, _directionChange: undefined, img: undefined, ready: true})));
+		res.json(Base.gamemodes.ffa.players.map(v => Object.assign(v, {id: undefined, _directionChange: undefined, img: undefined, ready: true})));
 	}
 	
 	static get info() {
