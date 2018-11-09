6dde66f3cb252ccaff450a03040485afa13e360a
y21 <artur.fiedler54@gmail.com>
2018-11-09 13:49:48 +0100

    Add API Controller

---
+++ backend/api/Controller.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+module.exports = class APIController {
+	constructor(app, route = "/api/") {
+		if (typeof route !== "string" || typeof app === "undefined") throw new ReferenceError("Please pass a route string and an express app object as constructor parameter.");
+		this.routes = fs.readdirSync(__dirname + "/endpoints/").map(v => {
+			const endpoint = require(`./endpoints/${v}`);
+			return Object.assign(endpoint.info, {run: endpoint.run})
+		});
+		this.app = app;
+		this.route = route;
+	}
+	
+	init(httpm) {
+		for(const endpoint of this.routes) {
+			this.app[httpm](this.route + endpoint.path, endpoint.run);
+		}
+	}
+};
