e821715b56d8d4d51843499de4eb956e2c53699b
y21 <artur.fiedler54@gmail.com>
2018-11-10 21:29:17 +0100

    Ban checks

--- backend/events/login.js
+++ backend/events/login.js
@@ -6,50 +6,60 @@ class loginEvent {};
  * @returns {undefined}
  */
 loginEvent.run = (...args) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const [res, io, data, sqlite, bcrypt, sessions, displayError] = args;
         // If username/password is undefined
         if (!res.username || !res.password) return io.to(data.id).emit("login", {
             status: 400,
-            message: "Either username or password is undefined."
+            message: "Please enter a valid username and password."
         });
-        sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
-            prepare.get([res.username]).then(result => {
-                if (!result) return displayError("Incorrect username or password.", data, "login", 400, io);
-                if (bcrypt.compareSync(res.password, result.password)) {
-                    sessions.getSession(sqlite, {
-                        type: "username",
-                        value: res.username
-                    }).then(session => {
-                        if (session) {
-                            sessions.deleteSession(sqlite, {
-                                type: "username",
-                                value: res.username
-                            }).catch(console.log);
-                        }
-                        sessions.registerID(sqlite, res.username).then(id => {
-                            io.to(data.id).emit("login", {
-                                status: 200,
-                                message: "Successfully logged in.",
-                                session_id: id
-                            });
-                            resolve(id);
-                        }).catch(error => {
-                            displayError(error.toString(), data, "login", 500, io);
-                            reject(error);
-                        });
-                    });
-                } else {
-                    displayError("Incorrect username or password.", data, "login", 400, io);
-                }
+        let banned = false;
+        await sqlite.prepare("SELECT reason, expires FROM bans WHERE username=?").then(async prepare => {
+            return await prepare.get([res.username]).then(result => {
+                if (typeof result === "undefined") return;
+                if (Date.now() > Number(result.expires)) return sqlite.prepare("DELETE FROM bans WHERE username=?").then(prepared => prepared.run([res.username]));
+                banned = true;
+                io.to(data.id).emit("login", {
+                    status: 403,
+                    message: `You have been banned: "${result.reason}". This ban expires at ${new Date(Number(result.expires)).toLocaleString()}`
+                });
+                return;
             });
-        }).catch(err => {
-            console.log(err)
-            if (err.toString().includes("no such table: accounts")) {
-                displayError("A problem occured on the server-side.", data, "register", 500, io);
-                sqlite.run("CREATE TABLE accounts (`username` TEXT, `password` TEXT, `br` INTEGER)").catch(console.log);
-            }
         });
+
+        if (banned === false) {
+            sqlite.prepare("SELECT * FROM accounts WHERE username = ?").then(prepare => {
+                prepare.get([res.username]).then(result => {
+                    if (!result) return displayError("Incorrect username or password.", data, "login", 400, io);
+                    if (bcrypt.compareSync(res.password, result.password)) {
+                        sessions.getSession(sqlite, {
+                            type: "username",
+                            value: res.username
+                        }).then(session => {
+                            if (session) {
+                                sessions.deleteSession(sqlite, {
+                                    type: "username",
+                                    value: res.username
+                                }).catch(console.log);
+                            }
+                            sessions.registerID(sqlite, res.username).then(id => {
+                                io.to(data.id).emit("login", {
+                                    status: 200,
+                                    message: "Successfully logged in.",
+                                    session_id: id
+                                });
+                                resolve(id);
+                            }).catch(error => {
+                                displayError(error.toString(), data, "login", 500, io);
+                                reject(error);
+                            });
+                        });
+                    } else {
+                        displayError("Incorrect username or password.", data, "login", 400, io);
+                    }
+                });
+            }).catch(() => {});
+        }
     });
 };
 
