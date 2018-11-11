3c34f0cdacdd47f48b70c53033a0bd3bc2d75828
y21 <artur.fiedler54@gmail.com>
2018-11-11 12:37:21 +0100

    Semicolon added

--- backend/events/ffaCoordinateChange.js
+++ backend/events/ffaCoordinateChange.js
@@ -5,11 +5,10 @@ const {
 
 ffaCoordinateChangeEvent.run = async (...args) => {
     const [eventd, data, io, Base, sqlite] = args;
-    // TODO: checks...
     eventd.id = data.id;
     if (parseInt(eventd.x) === NaN || parseInt(eventd.y) === NaN || parseInt(eventd.br) === NaN) return;
     try {
-        let prev = Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === eventd.owner)]
+        let prev = Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === eventd.owner)];
         eventd.lastnom = prev.lastnom;
         eventd._directionChange = prev._directionChange;
     } catch (e) {}
