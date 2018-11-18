52d1861a5c1cf5774a4408020316695aaeae4082
y21 <artur.fiedler54@gmail.com>
2018-11-18 01:59:35 +0100

    remove unnecessary js files

--- public/js/canvas.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.getElementsByTagName("canvas")[0];
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth / 1.25;
-canvas.height = window.innerHeight / 1.25;
-ctx.font = "20px arial";
-ctx.fillStyle = "white";
