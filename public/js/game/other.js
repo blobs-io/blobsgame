6ba4a72e9cc4d9c85816ce84d790780b13df2917
Timo <artur.fiedler54@gmail.com>
2018-11-21 20:14:54 +0100

    assign true to ownBlob.guest if guest GET parameter is set

--- public/js/game/other.js
+++ public/js/game/other.js
@@ -7,6 +7,9 @@ for (const blob of blobs) {
 const ownBlob = new BlobObj();
 ownBlob.ready = false;
 ownBlob.setBlob().then(() => ownBlob.display(true, true));
+if (/[\?\&]guest=true/.test(window.location.search)) {
+    ownBlob.guest = true;
+}
 
 // Initialize blob images
 objects.images.blobnom = new Image();
