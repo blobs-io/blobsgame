0e37d040cf05b7f6219ea20ea786073e3429fb6a
Timo <artur.fiedler54@gmail.com>
2018-11-25 19:02:51 +0100

    Format time in error message (daily coins)

--- backend/events/receiveDailyBonus.js
+++ backend/events/receiveDailyBonus.js
@@ -1,12 +1,16 @@
 class receiveDailyBonus { };
 
+function formatDate(ms) {
+    return Math.floor(ms/1000/60/60) + " hours and " + Math.floor(((ms - (1000 * 60 * 60 * Math.floor(ms/1000/60/60)))/1000/60)) + " minutes";
+}
+
 receiveDailyBonus.run = async (data, io, sockets, sqlite) => {
 	const targetSocket = sockets.find(v => v.socketid === data.id);
 	const userData = await require("../utils/getDataFromPlayer")(targetSocket.username, sqlite);
 	if (typeof targetSocket === "undefined") return;
 	if (Date.now() - userData.lastDailyUsage <= 86400000) return io.to(data.id).emit("alert", {
 			type: "error",
-			message: "Please wait " + ((86400000 - (Date.now() - userData.lastDailyUsage)) / 1000 / 60 / 60).toFixed(2) + " more hours."
+			message: "Please wait " + (formatDate(86400000 - (Date.now() - userData.lastDailyUsage))) + "."
 	});
 	const prepared = await sqlite.prepare("UPDATE accounts SET lastDailyUsage = '" + Date.now() + "', blobcoins = blobcoins + 20 WHERE username=?");
 	await prepared.run([targetSocket.username]);
