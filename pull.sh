16ad3c213683f804d2ccee46e8c78f5d0eb886e6
y21 <artur.fiedler54@gmail.com>
2018-12-02 00:11:58 +0100

    print stdout for debugging notes

--- pull.sh
+++ pull.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/blobs-io/blobs.io.git
+echo '--- Successfully cloned the repository ---';
 rm -rf ./backend ./public
 mv blobs.io/* ./
 rm -rf blobs.io
+echo '--- Old data has been overwritten. ---';
+echo '--- Make sure to change the host in frontend JavaScript files! ---';
