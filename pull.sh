dd257c6d8e8f791e11c400b9d9d6837cb806b5f5
Timo <artur.fiedler54@gmail.com>
2018-12-02 19:47:53 +0100

    kill node process after pulling (shell script)

--- pull.sh
+++ pull.sh
@@ -5,5 +5,6 @@ echo '--- Successfully cloned the repository ---';
 rm -rf ./backend ./public
 mv blobs.io/* ./
 rm -rf blobs.io
+pkill node
 echo '--- Old data has been overwritten. ---';
 echo '--- Make sure to change the host in frontend JavaScript files! ---';
