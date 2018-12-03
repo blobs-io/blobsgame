7738e27611fc7a0fbd36faa4e836d529dcddec6e
Timo <artur.fiedler54@gmail.com>
2018-12-03 21:39:19 +0100

    use Base.sockets not sockets call-by-value copy

--- backend/events/disconnect.js
+++ backend/events/disconnect.js
@@ -7,16 +7,16 @@ class disconnectEvent {};
  * @returns {Array} The sockets array
  */
 disconnectEvent.run = (...args) => {
-    const [sockets, data, Base, io] = args;
+    const [data, Base, io] = args;
     if (Base.gamemodes.ffa.players.some(v => v.id === data.id)) {
         io.sockets.emit("ffaPlayerDelete", Base.gamemodes.ffa.players.find(v => v.id === data.id).owner);
         Base.gamemodes.ffa.players.splice(Base.gamemodes.ffa.players.findIndex(v => v.id === data.id), 1);
     }
-    if (sockets.find(val => val.socketid === data.id)) {
-        sockets[sockets.findIndex(val => val.socketid === data.id)].inactiveSince = Date.now();
+    if (Base.sockets.find(val => val.socketid === data.id)) {
+        Base.sockets[Base.sockets.findIndex(val => val.socketid === data.id)].inactiveSince = Date.now();
     }
     return {
-        sockets,
+        sockets: Base.sockets,
         players: Base.gamemodes.ffa.players
     };
 };
