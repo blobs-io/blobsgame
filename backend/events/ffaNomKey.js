b7a9540f3dcf8df8a434d59717eb537912b92d6f
Timo <30553356+y21@users.noreply.github.com>
2018-12-07 23:00:02 +0100

    Fix bug where sometimes winner and loser are being swapped

--- backend/events/ffaNomKey.js
+++ backend/events/ffaNomKey.js
@@ -1,27 +1,26 @@
-class ffaNomKey { };
+class ffaNomKey {};
 const {
     execSync
 } = require("child_process");
 
 
-ffaNomKey.run = async(data, io, Base, sqlite) => {
-	const eventd = Base.gamemodes.ffa.players.find(v => v.id === data.id);
-	if (!eventd) return;
-	if (parseInt(eventd.x) === NaN || parseInt(eventd.y) === NaN || parseInt(eventd.br) === NaN) return;
-	
-	for (const blobobj of Base.gamemodes.ffa.players) {
+ffaNomKey.run = async (data, io, Base, sqlite) => {
+    const eventd = Base.gamemodes.ffa.players.find(v => v.id === data.id);
+    if (!eventd) return;
+    if (parseInt(eventd.x) === NaN || parseInt(eventd.y) === NaN || parseInt(eventd.br) === NaN) return;
+
+    for (const blobobj of Base.gamemodes.ffa.players) {
         if (eventd.owner !== blobobj.owner) {
             if (eventd.x < (blobobj.x + 30) && eventd.x > (blobobj.x - 30)) {
                 if (eventd.y < (blobobj.y + 30) && eventd.y > (blobobj.y - 30)) {
                     if (eventd.guest === true || blobobj.guest === true) return;
-					if (Date.now() - eventd.lastnom < 1500) return; // Nom cooldown (1.5 seconds)
+                    if (Date.now() - eventd.lastnom < 1500) return; // Nom cooldown (1.5 seconds)
                     // If blob is nommed
                     Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.id === data.id)].lastnom = Date.now();
 
 
-                    let won = Base.gamemodes.ffa.players.find(v => v.owner === eventd.owner)._directionChange > blobobj._directionChange;
-                    let winner = !won ? blobobj : Base.gamemodes.ffa.players.find(v => v.owner === eventd.owner);
-                    let loser = won ? blobobj : Base.gamemodes.ffa.players.find(v => v.owner === eventd.owner);
+                    let winner = eventd;
+                    let loser = blobobj;
 
                     if (eventd.br === blobobj.br) eventd.br -= 1;
                     if (parseInt(blobobj.br) !== NaN) {
@@ -31,14 +30,14 @@ ffaNomKey.run = async(data, io, Base, sqlite) => {
                         Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === loser.owner)].x = Math.floor(Math.random() * 150) + 150;
                         Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === loser.owner)].y = Math.floor(Math.random() * 150) + 150;
                         Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === loser.owner)].br = (loser.br - result <= 0 ? 1 : loser.br - result);
-                        
+
                         loser.br = Base.gamemodes.ffa.players.find(v => v.owner === loser.owner).br;
                         winner.br = Base.gamemodes.ffa.players.find(v => v.owner === winner.owner).br;
-                        
+
                         await sqlite.prepare("UPDATE accounts SET br=? WHERE username=?").then(v => v.run([(loser.br - result <= 0 ? 1 : loser.br - result), loser.owner]));
                         await sqlite.prepare("UPDATE accounts SET br=? WHERE username=?").then(v => v.run([(winner.br + result > 9999 ? 9999 : winner.br + result), winner.owner]));
-                        
-                        
+
+
                         io.sockets.emit("ffaPlayerNommed", {
                             winner,
                             loser,
