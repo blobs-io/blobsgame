e12e122d31f5a9f8209d87ff45731a2264994e21
Timo <30553356+y21@users.noreply.github.com>
2018-12-07 23:52:11 +0100

    Also include people in FFA in requestOnlineCount event

--- backend/server.js
+++ backend/server.js
@@ -115,7 +115,7 @@ io.on("connection", data => {
         data.on("ffaNomKey", () => require("./events/ffaNomKey").run(data, io, Base, sqlite));
 
         // Other events
-        data.on("requestOnlineCount", () => io.to(data.id).emit("onlineCount", Base.sockets.filter(v => v.inactiveSince === null).length));
+	data.on("requestOnlineCount", () => io.to(data.id).emit("onlineCount", Base.sockets.filter(v => v.inactiveSince === null).concat(Base.gamemodes.ffa.players).length));
         data.on("getCaptcha", () => require("./events/getCaptcha").run(sessions, io, data, captchas).then(res => captchas = res));
         data.on("login", res => require("./events/login").run(res, io, data, sqlite, bcrypt, sessions, utils.displayError));
         data.on("register", res => require("./events/register").run(res, io, data, utils.displayError, captchas, bcrypt, sqlite));
