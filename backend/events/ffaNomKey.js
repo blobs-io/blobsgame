bbd5cdf47199d626b479adccb9ff21eff42b6938
Timo <30553356+y21@users.noreply.github.com>
2018-12-08 16:09:27 +0100

    Add to recentPromotions table if promoted

--- backend/events/ffaNomKey.js
+++ backend/events/ffaNomKey.js
@@ -3,6 +3,49 @@ const {
     execSync
 } = require("child_process");
 
+function getTier(br) {
+    let result = {};
+    if (br >= 0 && br < 1500) {
+        result.tier = "bronze";
+        result.colorCode = "b57156";
+        result.emblemFile = "emblem_bronze.png";
+    } else if (br >= 1500 && br < 3000) {
+        result.tier = "silver";
+        result.colorCode = "dbdbdb";
+        result.emblemFile = "emblem_silver.png";
+    } else if (br >= 3000 && br < 5000) {
+        result.tier = "platinum";
+        result.colorCode = "E5E4E2";
+        result.emblemFile = "emblem_platinum.png";
+    } else if (br >= 5000 && br < 8000) {
+        result.tier = "gold";
+        result.colorCode = "D7AF00";
+        result.emblemFile = "emblem_gold.png";
+    } else if (br >= 8000 && br < 9500) {
+        result.tier = "diamond";
+        result.colorCode = "16f7ef";
+        result.emblemFile = "emblem_diamond.png";
+    } else if (br >= 9500 && br < 10000) {
+        result.tier = "painite";
+        result.colorCode = "16f77f";
+        result.emblemFile = "emblem_painite.png";
+    }
+    return result;
+}
+
+function promotedTo(oldbr, newbr) {
+	if (oldbr >= 1500 && newbr < 1500) return { drop: true, newTier: "bronze" };
+	if (oldbr < 1500 && newbr >= 1500) return { drop: false, newTier: "silver" };
+	if (oldbr >= 3000 && newbr < 3000) return { drop: true, newTier: "silver" };
+	if (oldbr < 3000 && newbr >= 3000) return { drop: false, newTier: "platinum" };
+	if (oldbr >= 5000 && newbr < 5000) return { drop: true, newTier: "platinum" };
+	if (oldbr < 5000 && newbr >= 5000) return { drop: false, newTier: "gold" };
+	if (oldbr >= 8000 && newbr < 8000) return { drop: true, newTier: "gold" };
+	if (oldbr < 8000 && newbr >= 8000) return { drop: false, newTier: "diamond" };
+	if (oldbr >= 9500 && newbr < 9500) return { drop: true, newTier: "diamond" };
+	if (oldbr < 9500 && newbr >= 9500) return { drop: false, newTier: "painite" };
+	return undefined;
+}
 
 ffaNomKey.run = async (data, io, Base, sqlite) => {
     const eventd = Base.gamemodes.ffa.players.find(v => v.id === data.id);
@@ -36,8 +79,22 @@ ffaNomKey.run = async (data, io, Base, sqlite) => {
 
                         await sqlite.prepare("UPDATE accounts SET br=? WHERE username=?").then(v => v.run([(loser.br - result <= 0 ? 1 : loser.br - result), loser.owner]));
                         await sqlite.prepare("UPDATE accounts SET br=? WHERE username=?").then(v => v.run([(winner.br + result > 9999 ? 9999 : winner.br + result), winner.owner]));
-
-
+						
+						const dropRes = {
+							winner: promotedTo(winner.br - result, winner.br) || {drop: undefined},
+							loser: promotedTo(loser.br + result, loser.br) || {drop: undefined}
+						};
+						if (typeof dropRes.winner.drop !== "undefined") {
+							sqlite.prepare("INSERT INTO recentPromotions VALUES (?, ?, ?, ?)").then(prepared => {
+								prepared.run([ winner.owner, dropRes.winner.newTier, dropRes.winner.drop, Date.now()]);
+							});
+						} else if (typeof dropRes.loser.drop !== "undefined") {
+							sqlite.prepare("INSERT INTO recentPromotions VALUES (?, ?, ?, ?)").then(prepared => {
+								prepared.run([ loser.owner, dropRes.loser.newTier, dropRes.loser.drop, Date.now()]);
+							});
+						}
+						
+						
                         io.sockets.emit("ffaPlayerNommed", {
                             winner,
                             loser,
