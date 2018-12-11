4e9ecec68196fb42c62b7578dc2784e9c42faa31
Timo <artur.fiedler54@gmail.com>
2018-12-11 17:11:15 +0100

    move request function to js/request.js

--- public/js/game/functions.js
+++ public/js/game/functions.js
@@ -122,24 +122,3 @@ function getTierByName(name) {
 		case "painite": return getTier(9999); break;
 	}
 }
-
-function request(url, method, headers) {
-    return new Promise((a,b) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url, true);
-        if (typeof headers === "object" && headers !== null) { // because typeof null === 'object'
-            for (const header of Object.entries(headers)) {
-                xhr.setRequestHeader(header[0], header[1]);
-            }
-        }
-        xhr.onload = () => {
-            if (xhr.readyState === 4) {
-                let statusHeader = xhr.getResponseHeader("status");
-                if (statusHeader.includes(",")) statusHeader = statusHeader.split(",")[1].substr(1);
-                if (xhr.status === 200 && statusHeader === "200") a(xhr);
-                else b(xhr);
-            }
-        };
-        xhr.send(null);
-    });
-}
