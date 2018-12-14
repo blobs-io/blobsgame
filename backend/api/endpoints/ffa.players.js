1264d0af4b73ad0eacdde544ff1348c79caf2bf4
Timo <artur.fiedler54@gmail.com>
2018-12-14 19:25:38 +0100

    Set status header to 200

--- backend/api/endpoints/ffa.players.js
+++ backend/api/endpoints/ffa.players.js
@@ -3,6 +3,7 @@ const Base = require("../../Base");
 module.exports = class Players {
 	static run(...data) {
 		const [req, res] = data;
+		res.set("status", 200);
 		res.json(Base.gamemodes.ffa.players.map(v => Object.assign(v, {id: undefined, _directionChange: undefined, img: undefined, ready: true})));
 	}
 	
