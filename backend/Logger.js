bd7e0b7507c704f504e60465cb9a8e7113598989
Timo <artur.fiedler54@gmail.com>
2018-12-16 18:59:48 +0100

    Add optional requests parameter to Logger#log and setInterval

--- backend/Logger.js
+++ backend/Logger.js
@@ -13,15 +13,21 @@ module.exports = class Logger {
         return this._requests = value;
     }
 
-    async log() {
+    setInterval(callback, ms) {
+        setInterval(() => {
+            this.log().then(callback);
+        }, ms || 1000);
+    }
+
+    async log(requests = this.requests) {
         const result = await Base.sqlite.all("SELECT * FROM logs");
         if (!result.some(v => v.name === "total")) {
             await Base.sqlite.run("INSERT INTO logs VALUES ('total', 0)");
         } else if (!result.some(v => v.name === "ffa")) {
             await Base.sqlite.run("INSERT INTO logs VALUES ('ffa', 0)");
         }
-        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ this.requests.total, "total" ]));
-        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ this.requests.ffa, "ffa" ]));
-        return this.requests;
+        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.total, "total" ]));
+        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.ffa, "ffa" ]));
+        return requests;
     }
 };
