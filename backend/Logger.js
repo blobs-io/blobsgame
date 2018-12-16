cea993227a98eb2b1856dbbc020d357eb39214d5
Timo <artur.fiedler54@gmail.com>
2018-12-16 20:03:50 +0100

    post traffic to discord webhook

--- backend/Logger.js
+++ backend/Logger.js
@@ -1,8 +1,11 @@
 const Base = require("./Base.js");
+const fetch = require("node-fetch");
+const formdata = require("form-data");
 
 module.exports = class Logger {
-    constructor(requests = { total:0, htmlOnly: 0, ffa:0 }) {
+    constructor(discordAuth = {}, requests = { total:0, htmlOnly: 0, ffa:0 }) {
         this._requests = requests;
+        this.discordAuth = discordAuth;
     }
 
     get requests() {
@@ -19,6 +22,17 @@ module.exports = class Logger {
         }, ms || 1000);
     }
 
+    async postDiscord() {
+        const data = await Base.sqlite.all("SELECT * FROM logs");
+        const form = new formdata();
+        form.append("content", `**Total requests:** ${data.find(v => v.name === "total").amount}\n**Total FFA requests:** ${data.find(v => v.name === "ffa").amount}\n**HTML-only requests: **${data.find(v => v.name === "htmlOnly").amount}`);
+        const request = await fetch(`https://discordapp.com/api/webhooks/${this.discordAuth.id}/${this.discordAuth.token}`, {
+            method: "POST",
+            body: form
+        });
+        return request;
+    }
+
     async log(requests = this.requests) {
         const result = await Base.sqlite.all("SELECT * FROM logs");
         if (!result.some(v => v.name === "total")) {
@@ -30,9 +44,12 @@ module.exports = class Logger {
         if (!result.some(v => v.name === "htmlOnly")) {
             await Base.sqlite.run("INSERT INTO logs VALUES ('htmlOnly', 0)");
         }
-        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.total, "total" ]));
-        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.ffa, "ffa" ]));
-        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.htmlOnly, "htmlOnly" ]));
+        await Base.sqlite.prepare("UPDATE logs SET amount = amount + ? WHERE name=?").then(v => v.run([ requests.total, "total" ]));
+        await Base.sqlite.prepare("UPDATE logs SET amount = amount + ? WHERE name=?").then(v => v.run([ requests.ffa, "ffa" ]));
+        await Base.sqlite.prepare("UPDATE logs SET amount = amount + ? WHERE name=?").then(v => v.run([ requests.htmlOnly, "htmlOnly" ]));
+        this.requests.total = 0;
+        this.requests.ffa = 0;
+        this.requests.htmlOnly = 0;
         return requests;
     }
 };
