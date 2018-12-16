615444866764ad4db74ab2bac68b057b40e27187
Timo <artur.fiedler54@gmail.com>
2018-12-16 19:22:28 +0100

    Implement htmlOnly log entry

--- backend/Logger.js
+++ backend/Logger.js
@@ -1,7 +1,7 @@
 const Base = require("./Base.js");
 
 module.exports = class Logger {
-    constructor(requests = { total:0, ffa:0 }) {
+    constructor(requests = { total:0, htmlOnly: 0, ffa:0 }) {
         this._requests = requests;
     }
 
@@ -23,11 +23,16 @@ module.exports = class Logger {
         const result = await Base.sqlite.all("SELECT * FROM logs");
         if (!result.some(v => v.name === "total")) {
             await Base.sqlite.run("INSERT INTO logs VALUES ('total', 0)");
-        } else if (!result.some(v => v.name === "ffa")) {
+        }
+        if (!result.some(v => v.name === "ffa")) {
             await Base.sqlite.run("INSERT INTO logs VALUES ('ffa', 0)");
+        }
+        if (!result.some(v => v.name === "htmlOnly")) {
+            await Base.sqlite.run("INSERT INTO logs VALUES ('htmlOnly', 0)");
         }
         await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.total, "total" ]));
         await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.ffa, "ffa" ]));
+        await Base.sqlite.prepare("UPDATE logs SET amount=? WHERE name=?").then(v => v.run([ requests.htmlOnly, "htmlOnly" ]));
         return requests;
     }
 };
