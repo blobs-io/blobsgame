965a8a1e93e12bad8c5812ff17a8d0b174616335
y21 <artur.fiedler54@gmail.com>
2018-12-17 22:23:53 +0100

    Add get/set accessors for x/y

--- backend/structures/Player.js
+++ backend/structures/Player.js
@@ -1,7 +1,7 @@
 module.exports = class Player {
 	constructor(x, y, owner, role = 0, blob = "blobowo") {
-		this.x = x;
-		this.y = y;
+		this._x = x;
+		this._y = y;
 		this.owner = owner;
 		this.br = 0;
 		this.blob = blob;
@@ -13,5 +13,25 @@ module.exports = class Player {
 		this.directionChangedAt = Date.now();
 		this.guest = false;
 		this.distance = 0;
+	}
+	
+	get x() {
+		if (this.direction === 1) return this.directionChangeCoordinates.x + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		else if (this.direction === 3) return this.directionChangeCoordinates.x - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		else return this.directionChangeCoordinates.x;
+	}
+	
+	set x(value) {
+		return this._x = value;
+	}
+	
+	get y() {
+		if (this.direction === 0) return this.directionChangeCoordinates.y - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		else if (this.direction === 2) return this.directionChangeCoordinates.y + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		else return this.directionChangeCoordinates.y;
+	}
+	
+	set y(value) {
+		return this._y = value;
 	}
 };
