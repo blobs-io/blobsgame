1ccf0eebd8dd4cc5f524dc19f44dfffc41ab5534
y21 <artur.fiedler54@gmail.com>
2018-12-17 18:59:44 +0100

    Use Base.rooms array instead

--- backend/events/ffaDirectionChange.js
+++ backend/events/ffaDirectionChange.js
@@ -2,7 +2,7 @@ class ffaDirectionChangeEvent {};
 
 ffaDirectionChangeEvent.run = (...args) => {
     const [eventd, data, io, Base] = args;
-    const target = Base.gamemodes.ffa.players[Base.gamemodes.ffa.players.findIndex(v => v.owner === eventd.owner)];
+    const target = Base.rooms.find(v => v.id === "ffa").players[Base.rooms.find(v => v.id === "ffa").players.findIndex(v => v.owner === eventd.owner)];
     if (target === undefined) return;
     target._directionChange = Date.now();
 };
