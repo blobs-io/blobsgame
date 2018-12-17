1e29751ea1d1e82e24bd3f9e03bfbad5a589d61c
y21 <artur.fiedler54@gmail.com>
2018-12-17 22:19:43 +0100

    Use Player class

--- backend/events/ffaDirectionChange.js
+++ backend/events/ffaDirectionChange.js
@@ -4,7 +4,14 @@ ffaDirectionChangeEvent.run = (...args) => {
     const [eventd, data, io, Base] = args;
     const target = Base.rooms.find(v => v.id === "ffa").players[Base.rooms.find(v => v.id === "ffa").players.findIndex(v => v.owner === eventd.owner)];
     if (target === undefined) return;
-    target._directionChange = Date.now();
+    if (typeof target.x !== "number" || typeof target.y !== "number") return;
+    target.directionChangedAt = Date.now();
+    target.direction = eventd._direction;
+    target.directionChangeCoordinates = {
+		x: eventd.directionChangeCoordinates.x,
+		y: eventd.directionChangeCoordinates.y
+	};
+	io.sockets.emit("ffaDirectionChanged", Object.assign(target, { id: undefined, x: target.x, y: target.y }));
 };
 
 module.exports = ffaDirectionChangeEvent;
