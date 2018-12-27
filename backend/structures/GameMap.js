b6a4aec4d606841f402b856601426e3efe6edeb5
Timo <artur.fiedler54@gmail.com>
2018-12-27 22:36:07 +0100

    Make GameMap-extended class and use id in it

--- backend/structures/GameMap.js
+++ backend/structures/GameMap.js
@@ -1,6 +1,5 @@
 module.exports = class GameMap {
     constructor(name, players = [], objects = { walls: [] }) {
-        this.id;
         this.map = name;
         this.players = players;
         this.objects = objects;
