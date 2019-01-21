e65c849f42047eb8fb86ba83409fe9e2658cde79
Timo <artur.fiedler54@gmail.com>
2019-01-21 20:20:07 +0100

    Use property shorthand

--- backend/events/getCaptcha.js
+++ backend/events/getCaptcha.js
@@ -10,14 +10,14 @@ getCaptchaEvent.run = (...args) => {
     return new Promise(resolve => {
         const captcha = sessions.generateSessionID().substr(0, 6);
         io.to(data.id).emit("captcha", {
-            captcha: captcha,
+            captcha,
             position: {
                 x: Math.floor(Math.random() * 150) + 25,
                 y: Math.floor(Math.random() * 65) + 25
             }
         });
         captchas.push({
-            captcha: captcha,
+            captcha,
             createdAt: Date.now()
         });
         resolve(captchas);
