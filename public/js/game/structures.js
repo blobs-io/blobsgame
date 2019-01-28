b5d705d46b7f9fc78d7abbe569391bcdd6d5b949
Timo <artur.fiedler54@gmail.com>
2019-01-28 18:03:43 +0100

    Make blobs slightly faster

--- public/js/game/structures.js
+++ public/js/game/structures.js
@@ -106,8 +106,8 @@ class BlobObj {
     
     get x() {
 		let x = this.directionChangeCoordinates.x;
-		if (this.direction === 1) x = this.directionChangeCoordinates.x + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
-		else if (this.direction === 3) x = this.directionChangeCoordinates.x - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		if (this.direction === 1) x = this.directionChangeCoordinates.x + (1.025 * ((Date.now() - this.directionChangedAt) / 10));
+		else if (this.direction === 3) x = this.directionChangeCoordinates.x - (1.025 * ((Date.now() - this.directionChangedAt) / 10));
 		if (x < 0) x = 0;
 		else if (x > 2000) x = 2000;
 		if (isInObject(x, this.previousY)) {
@@ -124,8 +124,8 @@ class BlobObj {
 	
 	get y() {
 		let y = this.directionChangeCoordinates.y;
-		if (this.direction === 0) y = this.directionChangeCoordinates.y - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
-		else if (this.direction === 2) y =  this.directionChangeCoordinates.y + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		if (this.direction === 0) y = this.directionChangeCoordinates.y - (1.025 * ((Date.now() - this.directionChangedAt) / 10));
+		else if (this.direction === 2) y =  this.directionChangeCoordinates.y + (1.025 * ((Date.now() - this.directionChangedAt) / 10));
 		if (y < 0) y = 0;
 		else if (y > 2000) y = 2000;
         if (isInObject(this.previousX, y)) {
