b995313bdd1c94755d4551669f86abbe91a81df8
Timo <artur.fiedler54@gmail.com>
2019-01-28 18:04:01 +0100

    Make blobs slightly faster (backend structure)

--- backend/structures/Player.js
+++ backend/structures/Player.js
@@ -31,8 +31,8 @@ module.exports = class Player {
 
 	get x() {
 		let x = this.directionChangeCoordinates.x;
-		if (this.direction === 1) x = this.directionChangeCoordinates.x + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
-		else if (this.direction === 3) x = this.directionChangeCoordinates.x - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		if (this.direction === 1) x = this.directionChangeCoordinates.x + (1.025 * ((Date.now() - this.directionChangedAt) / 10));
+		else if (this.direction === 3) x = this.directionChangeCoordinates.x - (1.025 * ((Date.now() - this.directionChangedAt) / 10));
 		if (x < 0) x = 0;
 		else if (x > this.maximumCoordinates.width) x = this.maximumCoordinates.width;
         if (isInObject(x, this.previousY, this.room.map.map.objects)) {
@@ -49,8 +49,8 @@ module.exports = class Player {
 	
 	get y() {
 		let y = this.directionChangeCoordinates.y;
-		if (this.direction === 0) y = this.directionChangeCoordinates.y - (1.025 * ((Date.now() - this.directionChangedAt) / 20));
-		else if (this.direction === 2) y = this.directionChangeCoordinates.y + (1.025 * ((Date.now() - this.directionChangedAt) / 20));
+		if (this.direction === 0) y = this.directionChangeCoordinates.y - (1.025 * ((Date.now() - this.directionChangedAt) / 10));
+		else if (this.direction === 2) y = this.directionChangeCoordinates.y + (1.025 * ((Date.now() - this.directionChangedAt) / 10));
 		if (y < 0) y = 0;
 		else if (y > this.maximumCoordinates.height) y = this.maximumCoordinates.height;
         if (isInObject(this.previousX, y, this.room.map.map.objects)) {
