fc519513fd536b9b7e2845d3c7a5e8ca774f3f18
Timo <artur.fiedler54@gmail.com>
2019-02-23 17:46:20 +0100

    (api) return wins/losses as well

--- backend/api/endpoints/players.js
+++ backend/api/endpoints/players.js
@@ -3,7 +3,7 @@ const { sqlite } = require("../../Base");
 module.exports = class Players {
 	static async run(...data) {
 		const [req, res] = data;
-		res.json(await sqlite.all("SELECT username, br, createdAt, role FROM accounts ORDER BY br DESC LIMIT 25"));
+		res.json(await sqlite.all("SELECT username, br, createdAt, role, wins, losses FROM accounts ORDER BY br DESC LIMIT 25"));
 	}
 	
 	static get info() {
