078ca6e2eac97c433ad7f1b46a2d406e12467be8
Timo <artur.fiedler54@gmail.com>
2019-03-25 18:31:32 +0100

    don't console.log

--- backend/events/ffaCoordinateChange.js
+++ backend/events/ffaCoordinateChange.js
@@ -5,9 +5,9 @@ ffaCoordinateChangeEvent.run = async (...args) => {
     if (parseInt(eventd.x) === NaN || parseInt(eventd.y) === NaN || parseInt(eventd.br) === NaN) return;
     try {
 		const room = Base.rooms[Base.rooms.findIndex(v => v.id === "ffa")];
-		if (!room) return console.log("room not found");
+		if (!room) return;
         let prev = room.players[room.players.findIndex(v => v.id === data.id)];
-        if (!prev) return console.log("id not found");
+        if (!prev) return;
         if((Math.abs(eventd.x - prev.x) > 50 || Math.abs(eventd.y - prev.y) > 50)) return io.to(data.id).emit("ffaUnauthorized");
         eventd.lastnom = prev.lastnom;
         eventd._directionChange = prev._directionChange;
