51a46a839d07f7196f5ce9eb6ac7789974eef8b8
Timo <artur.fiedler54@gmail.com>
2019-03-28 14:36:13 +0100

    Add ffaKickPlayer event file

---
+++ backend/events/ffaKickPlayer.js
@@ -0,0 +1,23 @@
+class ffaKickPlayerEvent {}
+
+ffaKickPlayerEvent.run = async (...args) => {
+    const [eventd, data, io, Base] = args;
+    const room = Base.rooms.find(v => v.id === "ffa");
+    const requester = room.players.find(v => v.id === data.id);
+    if (!requester) return;
+    if (!room) return;
+    if (requester.role !== 1) {
+        io.to(data.id).emit("ffaKick", "Insufficient permissions.");
+        return data.disconnect();
+    } else {
+        const target = room.players.find(v => v.owner === eventd.user);
+        if (target && typeof eventd.reason === "string") {
+            if (eventd.reason.length > 0 && eventd.reason.length < 128) {
+                io.to(target.id).emit("ffaKick", eventd.reason);
+                io.sockets.sockets[target.id].disconnect();
+            }
+        }
+    }
+};
+
+module.exports = ffaKickPlayerEvent;
