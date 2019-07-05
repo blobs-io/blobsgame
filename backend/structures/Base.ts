deeaa22130debe243b12ac0f1c0e3952790b6ca5
Timo <artur.fiedler54@gmail.com>
2019-07-05 23:58:13 +0200

    add base ts file

---
+++ backend/structures/Base.ts
@@ -0,0 +1,48 @@
+import * as express from "express";
+import * as ws from "ws";
+import sqlite from "sqlite";
+
+interface Server {
+    app: express.Application;
+    port: number;
+    readyCallback?: () => any;
+}
+
+interface BaseOptions {
+    server: Server;
+    wsServer: ws.Server;
+    database?: any;
+}
+
+
+export default class Base {
+    public server: Server;
+    public wsServer: ws.Server;
+    public db: any;
+
+    constructor(options: BaseOptions) {
+        this.server = options.server;
+        this.server.app.listen(options.server.port, options.server.readyCallback);
+        this.wsServer = options.wsServer;
+        this.db = options.database;
+    }
+
+    /**
+     * Initializes database
+     *
+     * @param {string} path
+     * @returns {Promise<undefined>}
+     */
+    async initializeDatabase(path: string) {
+        const { db } = this;
+        await db.open(path);
+        await db.run("CREATE TABLE IF NOT EXISTS logs (`name` TEXT, `amount` INTEGER)");
+        await db.run("CREATE TABLE IF NOT EXISTS clans (`name` TEXT, `leader` TEXT, `cr` INTEGER DEFAULT 0, `members` TEXT, `description` TEXT)");
+        await db.run("CREATE TABLE IF NOT EXISTS verifications (`user` TEXT, `code` TEXT, `requestedAt` TEXT)");
+        await db.run("CREATE TABLE IF NOT EXISTS recentPromotions (`user` TEXT, `newTier` TEXT, `drop` INTEGER, `promotedAt` TEXT)");
+        await db.run("CREATE TABLE IF NOT EXISTS news (`headline` TEXT, `content` TEXT, `createdAt` TEXT)");
+        await db.run("CREATE TABLE IF NOT EXISTS accounts (`username` TEXT, `password` TEXT, `br` INTEGER, `createdAt` TEXT, `role` INTEGER, `blobcoins` INTEGER, `lastDailyUsage` TEXT, `distance` INTEGER, blobs `TEXT`, `activeBlob` TEXT, `clan` TEXT, `wins` INTEGER, `losses` INTEGER)");
+        await db.run("CREATE TABLE IF NOT EXISTS sessionids (`username` TEXT, `sessionid` TEXT, `expires` TEXT)");
+        await db.run("CREATE TABLE IF NOT EXISTS bans (`username` TEXT, `reason` TEXT, `bannedAt` TEXT, `expires` TEXT, `moderator` TEXT)");
+    }
+}
