58fc647b06e732fe07f1c0492b484fd67d537cfd
Timo <artur.fiedler54@gmail.com>
2019-07-06 20:10:02 +0200

    send file for / route

--- backend/routes/root.ts
+++ backend/routes/root.ts
@@ -1,5 +1,6 @@
 import RouteInformation from "../structures/Route";
 import Base from "../structures/Base";
+import { readFile } from "fs";
 
 export default class Root {
     static route: RouteInformation = {
@@ -7,6 +8,12 @@ export default class Root {
     };
 
     static run(req: any, res: any, base: Base): void {
-        res.send("yes");
+        readFile("./public/index.html", "utf8", (e, r) => {
+            if (e) return res.status(500).json({
+                message: "An error occurred on the server (could not read file)"
+            });
+
+            res.send(r);
+        });
     }
 }
