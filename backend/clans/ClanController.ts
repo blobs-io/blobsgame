a2c273ba009e2215c05cce016ca5290f55782b42
Timo <artur.fiedler54@gmail.com>
2019-08-11 03:46:24 +0200

    rewrite clan stuff

---
+++ backend/clans/ClanController.ts
@@ -0,0 +1,36 @@
+import Base from "../structures/Base";
+import * as express from "express";
+import { readFileSync } from "fs";
+import Clan from "../structures/Clan";
+
+export default class ClanController {
+    public app: express.Application;
+    public base: Base;
+
+    constructor(app: express.Application, base: Base) {
+        this.app = app;
+        this.base = base;
+    }
+
+    listen(): void {
+        this.app.get("/clans/", (req: express.Request, res: express.Response) => {
+            res.send(
+                readFileSync(__dirname + "/index_anonymous.html", "utf8")
+            );
+        });
+        this.app.get("/clans/view/:clan", async (req: express.Request, res: express.Response) => {
+            if (!req.params.clan) return res.send("Please specify a clan");
+            const clan: Clan | undefined = await this.base.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
+            if (!clan) return res.send("Clan was not found");
+            res.send(
+                readFileSync(__dirname + "/clan_view_anonymous.html", "utf8")
+                    .replace(/{leader}/g, clan.leader)
+                    .replace(/{cr}/g, clan.cr.toString())
+                    .replace(/{members}/g, clan.members.split(",").join(", "))
+                    .replace(/\{description\}/g, (clan.description || "-")
+                        .replace(/</g, "&lt;")
+                        .replace(/>/g, "&gt;"))
+            );
+        });
+    }
+}
