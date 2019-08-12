3a6fe420768b304572ea9d3b9a9c3d23b1c7b972
Timo <artur.fiedler54@gmail.com>
2019-08-12 22:57:54 +0200

    add dateformatter

---
+++ backend/utils/DateFormatter.ts
@@ -0,0 +1,3 @@
+export function format(ms: number) {
+    return Math.floor(ms/1000/60/60) + " hours and " + Math.floor(((ms - (1000 * 60 * 60 * Math.floor(ms/1000/60/60)))/1000/60)) + " minutes";
+}
