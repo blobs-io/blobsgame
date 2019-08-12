f00035118ca49dae5c24a7034a8bd71f37731a58
Timo <artur.fiedler54@gmail.com>
2019-08-12 22:35:42 +0200

    db as parameter

--- backend/clans/ClanController.ts
+++ backend/clans/ClanController.ts
@@ -1,15 +1,14 @@
-import Base from "../structures/Base";
 import * as express from "express";
 import { readFileSync } from "fs";
 import Clan from "../structures/Clan";
 
 export default class ClanController {
     public app: express.Application;
-    public base: Base;
+    public db: any;
 
-    constructor(app: express.Application, base: Base) {
+    constructor(app: express.Application, db: any) {
         this.app = app;
-        this.base = base;
+        this.db = db;
     }
 
     listen(): void {
@@ -20,14 +19,14 @@ export default class ClanController {
         });
         this.app.get("/clans/view/:clan", async (req: express.Request, res: express.Response) => {
             if (!req.params.clan) return res.send("Please specify a clan");
-            const clan: Clan | undefined = await this.base.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
+            const clan: Clan | undefined = await this.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
             if (!clan) return res.send("Clan was not found");
             res.send(
                 readFileSync(__dirname + "/clan_view_anonymous.html", "utf8")
                     .replace(/{leader}/g, clan.leader)
                     .replace(/{cr}/g, clan.cr.toString())
                     .replace(/{members}/g, clan.members.split(",").join(", "))
-                    .replace(/\{description\}/g, (clan.description || "-")
+                    .replace(/description/g, (clan.description || "-")
                         .replace(/</g, "&lt;")
                         .replace(/>/g, "&gt;"))
             );
