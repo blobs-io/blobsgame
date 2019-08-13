80bfd18e311aeea7e774831ecf4a10d688503d73
Timo <artur.fiedler54@gmail.com>
2019-08-13 21:30:04 +0200

    rename things

--- backend/clans/ClanController.ts
+++ backend/clans/ClanController.ts
@@ -14,7 +14,7 @@ export default class ClanController {
     listen(): void {
         this.app.get("/clans/", (req: express.Request, res: express.Response) => {
             res.send(
-                readFileSync(__dirname + "/index_anonymous.html", "utf8")
+                readFileSync(__dirname + "/index_anon_templ.html", "utf8")
             );
         });
         this.app.get("/clans/view/:clan", async (req: express.Request, res: express.Response) => {
@@ -22,7 +22,7 @@ export default class ClanController {
             const clan: Clan | undefined = await this.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
             if (!clan) return res.send("Clan was not found");
             res.send(
-                readFileSync(__dirname + "/clan_view_anonymous.html", "utf8")
+                readFileSync(__dirname + "/clan_view_anon_templ.html", "utf8")
                     .replace(/{leader}/g, clan.leader)
                     .replace(/{cr}/g, clan.cr.toString())
                     .replace(/{members}/g, clan.members.split(",").join(", "))
