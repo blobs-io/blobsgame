35fe2a9dd6898bd451795f76ee7bc9ba9ae764b8
Timo <artur.fiedler54@gmail.com>
2019-08-13 02:09:47 +0200

    stupid shell script

--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/blobs-io/blobs.io.git
-echo '--- Successfully cloned the repository ---';
-rm -rf ./backend ./public ./maps
-mv blobs.io/* ./
-rm -rf blobs.io
-pkill node
-echo '--- Old data has been overwritten. ---';
