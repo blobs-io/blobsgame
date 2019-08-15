cdfa3f8dbaa87734b78da274eeef581c3231b46d
Timo <artur.fiedler54@gmail.com>
2019-08-16 00:02:12 +0200

    add wsSocket interface

--- backend/structures/Socket.ts
+++ backend/structures/Socket.ts
@@ -1,7 +1,13 @@
+import * as ws from "ws";
+
 export default interface Socket {
     username: string;
     br: number;
     role: number;
     guest: boolean;
     sessionid?: string;
+}
+export interface wsSocket {
+    conn: ws;
+    id: string;
 }
