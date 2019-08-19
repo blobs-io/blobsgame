c7fa68bf28eb6ff07cfb0fb9bcb49a6cc3f1908a
Timo <artur.fiedler54@gmail.com>
2019-08-19 22:55:21 +0200

    new tier in backend

--- backend/utils/TierHelper.ts
+++ backend/utils/TierHelper.ts
@@ -11,74 +11,62 @@ export interface Promotion {
 
 export function getTier(br: number): Tier {
     let result: Tier = {};
-    if (br >= 0 && br < 1500) {
+    if (br >= 0 && br < 2000) {
         result.tier = "bronze";
         result.colorCode = "b57156";
         result.emblemFile = "emblem_bronze.png";
-    } else if (br >= 1500 && br < 3000) {
+    } else if (br >= 2000 && br < 4000) {
         result.tier = "silver";
         result.colorCode = "dbdbdb";
         result.emblemFile = "emblem_silver.png";
-    } else if (br >= 3000 && br < 5000) {
+    } else if (br >= 4000 && br < 6000) {
         result.tier = "platinum";
         result.colorCode = "E5E4E2";
         result.emblemFile = "emblem_platinum.png";
-    } else if (br >= 5000 && br < 8000) {
+    } else if (br >= 6000 && br < 9000) {
         result.tier = "gold";
         result.colorCode = "D7AF00";
         result.emblemFile = "emblem_gold.png";
-    } else if (br >= 8000 && br < 9500) {
+    } else if (br >= 9000) {
         result.tier = "diamond";
         result.colorCode = "16f7ef";
         result.emblemFile = "emblem_diamond.png";
-    } else if (br >= 9500 && br < 10000) {
-        result.tier = "painite";
-        result.colorCode = "16f77f";
-        result.emblemFile = "emblem_painite.png";
     }
     return result;
 }
 
 export function promotedTo(oldbr: number, newbr: number): Promotion | void {
-    if (oldbr >= 1500 && newbr < 1500) return {
+    if (oldbr >= 2000 && newbr < 2000) return {
         drop: true,
         newTier: "bronze"
     };
-    if (oldbr < 1500 && newbr >= 1500) return {
+    if (oldbr < 2000 && newbr >= 2000) return {
         drop: false,
         newTier: "silver"
     };
-    if (oldbr >= 3000 && newbr < 3000) return {
+    if (oldbr >= 4000 && newbr < 4000) return {
         drop: true,
         newTier: "silver"
     };
-    if (oldbr < 3000 && newbr >= 3000) return {
+    if (oldbr < 4000 && newbr >= 4000) return {
         drop: false,
         newTier: "platinum"
     };
-    if (oldbr >= 5000 && newbr < 5000) return {
+    if (oldbr >= 6000 && newbr < 6000) return {
         drop: true,
         newTier: "platinum"
     };
-    if (oldbr < 5000 && newbr >= 5000) return {
+    if (oldbr < 6000 && newbr >= 6000) return {
         drop: false,
         newTier: "gold"
     };
-    if (oldbr >= 8000 && newbr < 8000) return {
+    if (oldbr >= 9000 && newbr < 9000) return {
         drop: true,
         newTier: "gold"
     };
-    if (oldbr < 8000 && newbr >= 8000) return {
+    if (oldbr < 9000 && newbr >= 9000) return {
         drop: false,
         newTier: "diamond"
-    };
-    if (oldbr >= 9500 && newbr < 9500) return {
-        drop: true,
-        newTier: "diamond"
-    };
-    if (oldbr < 9500 && newbr >= 9500) return {
-        drop: false,
-        newTier: "painite"
     };
     return undefined;
 }
