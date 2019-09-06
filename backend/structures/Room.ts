c369b4139d38d6e1a349d8382d08c40b7e5ae6af
Timo <artur.fiedler54@gmail.com>
2019-09-06 18:44:23 +0200

    add mode

--- backend/structures/Room.ts
+++ backend/structures/Room.ts
@@ -1,12 +1,19 @@
 import GameMap from "./GameMap";
 import Player from "./Player";
 
+export const Mode: any = {
+    FFA: "ffa",
+    ELIMINATION: "elimination"
+};
+
 export default class Room extends GameMap {
     public id: string;
     public players: Player[];
-    constructor(map: any = {}, id: string = Math.random().toString(32).substr(2,6)) {
+    public mode: string;
+    constructor(map: any = {}, id: string = Math.random().toString(32).substr(2,6), mode: string = Mode.FFA) {
         super(map);
         this.id = id;
+        this.mode = mode;
         this.players = [];
     }
 }
