44aac8ac95c558e5026b0ce60aa949009bd821b0
Timo <artur.fiedler54@gmail.com>
2019-09-07 19:58:59 +0200

    update room structure (add properties)

--- backend/structures/Room.ts
+++ backend/structures/Room.ts
@@ -10,10 +10,16 @@ export default class Room extends GameMap {
     public id: string;
     public players: Player[];
     public mode: string;
+    public createdAt: number;
     constructor(map: any = {}, id: string = Math.random().toString(32).substr(2,6), mode: string = Mode.FFA) {
         super(map);
         this.id = id;
         this.mode = mode;
         this.players = [];
+        this.createdAt = Date.now();
+    }
+
+    get uptime() {
+        return Date.now() - this.createdAt;
     }
 }
