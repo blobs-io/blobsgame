00439a1ddcd6dcef26b715fa0f0a7c4e11ca03b6
Timo <artur.fiedler54@gmail.com>
2019-09-07 19:58:28 +0200

    add eliminationroom structure

--- backend/structures/EliminationRoom.ts
+++ backend/structures/EliminationRoom.ts
@@ -1,15 +1,36 @@
 import * as Room from "./Room";
 
 export enum State {
-    WAITING
+    WAITING,
+    INGAME
 }
 
 export default class EliminationRoom extends Room.default {
-    constructor() {
+    static waitingTime: number = 300000;
+    static waitingTimeFull: number = 120000;
+    static minPlayersStartup: number = 4;
+    public state: State;
+    public _interval: NodeJS.Timeout;
+
+    constructor(state = State.WAITING) {
         super(undefined, undefined, Room.Mode.ELIMINATION);
+        this.state = state;
+        this._interval = setInterval(() => {
+            if (this.state === State.WAITING && this.startsAt >= Date.now()) {
+                clearInterval(this._interval);
+                this.start();
+            }
+        }, 1000);
+    }
+
+    get startsAt() {
+        return Date.now() + (this.players.length >= EliminationRoom.minPlayersStartup ?
+            EliminationRoom.waitingTimeFull :
+            EliminationRoom.waitingTime);
     }
 
     start() {
 
     }
+
 }
