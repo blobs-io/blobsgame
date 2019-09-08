bf156385f1ff42bb78912fc8fa5030e440b7c0c3
Timo <artur.fiedler54@gmail.com>
2019-09-08 23:12:04 +0200

    add broadcast and broadcastSend to Room class

--- backend/structures/Room.ts
+++ backend/structures/Room.ts
@@ -1,5 +1,8 @@
 import GameMap from "./GameMap";
 import Player from "./Player";
+import {wsSocket} from "./Socket";
+import {EventTypes, OPCODE} from "../WSEvents";
+import Base from "./Base";
 
 export const Mode: any = {
     FFA: "ffa",
@@ -11,15 +14,29 @@ export default class Room extends GameMap {
     public players: Player[];
     public mode: string;
     public createdAt: number;
-    constructor(map: any = {}, id: string = Math.random().toString(32).substr(2,6), mode: string = Mode.FFA) {
+    public base: Base;
+    constructor(base: Base, map: any = {}, id: string = Math.random().toString(32).substr(2,6), mode: string = Mode.FFA) {
         super(map);
         this.id = id;
         this.mode = mode;
         this.players = [];
         this.createdAt = Date.now();
+        this.base = base;
     }
 
     get uptime() {
         return Date.now() - this.createdAt;
+    }
+
+    broadcast(fn: (ws: wsSocket, player?: Player) => any): void {
+        for (let i: number = 0; i < this.players.length; ++i) {
+            const socket: wsSocket | undefined = this.base.wsSockets.find((v: wsSocket) => v.id === this.players[i].id);
+            if (!socket) continue;
+            fn(socket, this.players[i]);
+        }
+    }
+
+    broadcastSend(str: string) {
+        this.broadcast(ws => ws.conn.send(str));
     }
 }
