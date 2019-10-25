0d7960fbd64e8e6332814b85eac7a387e1e87ccb
Timo <artur.fiedler54@gmail.com>
2019-10-25 18:23:12 +0200

    remove unnecessary file

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This script can be used for deploying
-npm install
-npm install typescript
-node_modules/typescript/bin/tsc backend/Server.ts -t es5 --removeComments
-node_modules/typescript/bin/tsc public/js/Game.ts -t es5 --removeComments
