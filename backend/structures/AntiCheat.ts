58798d341df6e2f0d31c57e7e407a3961ab5c6b1
Timo <artur.fiedler54@gmail.com>
2019-10-25 20:48:42 +0200

    add comments to AntiCheat structure

--- backend/structures/AntiCheat.ts
+++ backend/structures/AntiCheat.ts
@@ -1,27 +1,40 @@
+// Used for detecting suspicious behavior
 export default class AntiCheat {
+    // Stores the flags
+    // Higher flag value = Higher probability of cheating
     public _FLAGS: number;
+
     constructor(flagValue: number = 0) {
+        // Assign local variable
         this._FLAGS = flagValue;
     }
 
+    // Equivalent to reading property this._FLAGS
+    // Just more convenient
     get flags(): number {
         return this._FLAGS;
     }
 
+    // Equivalent to assigning to property this._FLAGS
+    // Just more convenient
     set flags(value) {
         this._FLAGS = value;
     }
 
-    clear(): void {
+    // Resets flags to 0
+    public clear(): void {
         this.flags = 0;
     }
 
-    penalize(action: number, value: number) {
+    // Penalizes user by a calculated amount
+    public penalize(action: number, value: number): void {
         const penalty: number = AntiCheat.penalize(action, value);
         this.flags += penalty;
     }
 
-    static penalize(action: number, value: number) {
+    // Penalizes an action by a calculated number
+    // No instance needed for this
+    public static penalize(action: number, value: number) {
         let penalty = 0;
         if (action === 1) {
             if (value > 10 && value < 20) penalty += 0x1;
