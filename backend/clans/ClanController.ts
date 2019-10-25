68b9a5a4d5757b1a71f8e37e5716bca04be99451
Timo <artur.fiedler54@gmail.com>
2019-10-25 20:30:59 +0200

    add comments to clancontroller

--- backend/clans/ClanController.ts
+++ backend/clans/ClanController.ts
@@ -1,22 +1,34 @@
+// Imports
 import * as express from "express";
 import { readFileSync } from "fs";
 import Clan from "../structures/Clan";
 
+// Used for listening to requests that are related to clans
 export default class ClanController {
+    // A reference to the express router
     public app: express.Application;
+    // A reference to the SQLite driver (base.db)
     public db: any;
 
     constructor(app: express.Application, db: any) {
+        // Assign local variables to object
         this.app = app;
         this.db = db;
     }
 
-    listen(): void {
+    // This function may only be called once
+    // It creates listeners for every endpoint
+    public listen(): void {
+        // GET /clans/
+        // Returns the clans page (a list of some existing clans)
         this.app.get("/clans/", (req: express.Request, res: express.Response) => {
             res.send(
                 readFileSync(__dirname + "/index_anon_templ.html", "utf8")
             );
         });
+
+        // GET /clans/view/:clan
+        // Retrieve information about a specific clan
         this.app.get("/clans/view/:clan", async (req: express.Request, res: express.Response) => {
             if (Array.isArray(req.params)) return;
             if (!req.params.clan) return res.send("Please specify a clan");
