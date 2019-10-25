24a3f12b31935ce90e6b90dc0cf41a175d7d55d3
Timo <artur.fiedler54@gmail.com>
2019-10-25 21:34:34 +0200

    add comments to clan class

--- backend/structures/Clan.ts
+++ backend/structures/Clan.ts
@@ -1,10 +1,16 @@
+// Used to represent a clan
 export default class Clan {
+    // The leader of this clan
     public leader: string;
+    // The rating of this clan (signed)
     public cr: number;
+    // Raw string of all members (split by , to get an array)
     public members: string;
+    // The clan description
     public description: string;
 
     constructor(leader: string, cr: number, members: string, description: string) {
+        // Store local variables
         this.leader = leader;
         this.cr = cr;
         this.members = members;
