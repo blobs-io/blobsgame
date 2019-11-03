1e252e2f39d1e91ce075e48f23b466402ec6fb4e
Timo <artur.fiedler54@gmail.com>
2019-11-03 23:33:12 +0100

    dont make it an enum

--- backend/structures/Item.ts
+++ backend/structures/Item.ts
@@ -1,17 +1,17 @@
-export enum ItemType {
+export const ItemType: any = {
     // Health item; fills up health points
-    HEALTH = 0,
+    HEALTH: 0,
     // Adds blobcoin(s) to the user
-    COIN   = 1
-}
+    COIN: 1
+};
 
 export interface Item {
     // x coordinate of this item
     x: number;
     // y coordinate of this item
     y: number;
-    // Type of this item
-    type: ItemType;
+    // Type of this item (see ItemType)
+    type: number;
     // Unique identifier for this item
     id: string;
 }
