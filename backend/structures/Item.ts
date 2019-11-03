cc3338a3f97fb82ebce6aef5bf986be24acb9eb0
Timo <artur.fiedler54@gmail.com>
2019-11-03 23:15:19 +0100

    expand item structure

--- backend/structures/Item.ts
+++ backend/structures/Item.ts
@@ -1,8 +1,17 @@
 export enum ItemType {
+    // Health item; fills up health points
     HEALTH = 0,
+    // Adds blobcoin(s) to the user
     COIN   = 1
 }
 
 export interface Item {
-
+    // x coordinate of this item
+    x: number;
+    // y coordinate of this item
+    y: number;
+    // Type of this item
+    type: ItemType;
+    // Unique identifier for this item
+    id: string;
 }
