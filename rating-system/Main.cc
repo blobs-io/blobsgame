287494842269ad1b58fd655158f88963e17de24c
Timo <artur.fiedler54@gmail.com>
2019-11-10 20:52:20 +0100

    add argument checks

--- rating-system/Main.cc
+++ rating-system/Main.cc
@@ -4,6 +4,22 @@
 void Calculate(const v8::FunctionCallbackInfo<v8::Value>& args) {
     v8::Isolate* isolate = args.GetIsolate();
 
+    if (args.Length() != 2) {
+        isolate->ThrowException(
+            v8::Exception::TypeError(
+                v8::String::NewFromUtf8(isolate, "Invalid number of arguments. Required: 2", v8::NewStringType::kNormal).ToLocalChecked()
+            )
+        );
+    }
+
+    if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+        isolate->ThrowException(
+            v8::Exception::TypeError(
+                v8::String::NewFromUtf8(isolate, "Both arguments have to be numbers", v8::NewStringType::kNormal).ToLocalChecked()
+                )
+            );
+        return;
+    }
     int ownBR = args[0].As<v8::Number>()->Value(),
     opponentBR = args[1].As<v8::Number>()->Value();
 
