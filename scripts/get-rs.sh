57c443be0abf68f2d8c9d22fa99e7367eefdc269
Timo <artur.fiedler54@gmail.com>
2019-11-10 19:22:48 +0100

    update script

--- scripts/get-rs.sh
+++ scripts/get-rs.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-
-# clone the rating-system repository
 git clone https://github.com/blobs-io/rating-system --quiet
-
-# navigate into cloned repo
-cd rating-system
-
-# make deps.sh file executable
-chmod 777 deps.sh
-
-# execute deps shell script to download required dependencies
-./deps.sh
-
-# return to scripts folder
-cd ..
-
-# compile rating-system to binary in project folder
-g++ -std=c++11 -Irating-system/include rating-system/main.cc -o b
-
-# Make directory 'include'
 mkdir ../rating-system/include
-
-# Move algorithm file to ../rating-system/include so the node addon can use it
 mv rating-system/algorithm.cc ../rating-system/include/algorithm.cc
-
-# remove repository (only the binary is needed)
 rm -rf rating-system
-
-# move executable file to main folder
-mv b ../
