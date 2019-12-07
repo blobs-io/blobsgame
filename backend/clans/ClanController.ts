7c53690c3f1cd35166832f5d78b4a1c023e2a03b
Timo <artur.fiedler54@gmail.com>
2019-12-07 19:46:31 +0000

    change to ClanData interface

--- backend/clans/ClanController.ts
+++ backend/clans/ClanController.ts
@@ -1,7 +1,7 @@
 // Imports
 import * as express from "express";
 import { readFileSync } from "fs";
-import Clan from "../structures/Clan";
+import { ClanData } from "../structures/Clan";
 
 // Used for listening to requests that are related to clans
 export default class ClanController {
@@ -34,12 +34,12 @@ export default class ClanController {
         this.app.get("/clans/view/:clan", async (req: express.Request, res: express.Response) => {
             if (Array.isArray(req.params)) return;
             if (!req.params.clan) return res.send("Please specify a clan");
-            const clan: Clan | undefined = await this.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
+            const clan: ClanData | undefined = await this.db.get("SELECT * FROM clans WHERE name=?", req.params.clan);
             if (!clan) return res.send("Clan was not found");
             res.send(
                 readFileSync(__dirname + "/clan_view_anon_templ.html", "utf8")
                     .replace(/{leader}/g, clan.leader)
-                    .replace(/{cr}/g, clan.cr.toString())
+                    .replace(/{cr}/g, String(clan.cr))
                     .replace(/{members}/g, clan.members.split(",").join(", ").slice(1, -1))
                     .replace(/{description}/g, (clan.description || "-")
                         .replace(/</g, "&lt;")
