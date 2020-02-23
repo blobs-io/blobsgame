7f11373ada1ca9989ba1eda5bca196d61a49ad67
Timo <btimo8822@gmail.com>
2020-02-23 23:52:19 +0100

    add database

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	// postgres driver
+	_ "github.com/lib/pq"
+)
+
+type DbConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	DBName   string `json:"dbname"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	SSL      string `json:"ssl"`
+}
+
+// Database is the database driver
+var Database *sql.DB
+
+// Init initializes SQL connection
+func Init(config DbConfig) error {
+	Database, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=%s", config.Host, config.Port, config.DBName, config.User, config.Password, config.SSL))
+	if err != nil {
+		return err
+	}
+	return nil
+}
