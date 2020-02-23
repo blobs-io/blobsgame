0eaa2c8ef8ab65849408523eebd5f43f014f4444
Timo <btimo8822@gmail.com>
2020-02-23 23:50:16 +0100

    database initialization

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/blobs-io/blobsgame/database"
 	"github.com/blobs-io/blobsgame/http/web"
 	"github.com/blobs-io/blobsgame/utils/config"
 	"os/exec"
@@ -15,13 +16,19 @@ func main() {
 	}
 	fmt.Printf("Running hash %s\n", res)
 
-	// Parse config file
-	err = config.ParseConfig("configs/config.json")
+	// Parse config files
+	err = config.ParseMainConfig("configs/config.json")
 	if err != nil {
 		panic(err)
 	}
+	err = config.ParseDatabaseConfig("configs/database.json")
 
-
+	// Init database connection
+	err = database.Init(config.DatabaseConfig)
+	if err != nil {
+		panic(err)
+	}
+	
 	// Init web server
 	go web.Init(config.MainConfig.Port)
 }
