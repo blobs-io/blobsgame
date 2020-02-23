fbe6571222464978cc660c10ec61f971166616f6
Timo <btimo8822@gmail.com>
2020-02-23 23:58:10 +0100

    add upgrader

--- http/websockets/handler.go
+++ http/websockets/handler.go
@@ -1,5 +1,13 @@
 package websockets
 
+import "github.com/gorilla/websocket"
+
+var upgrader = websocket.Upgrader{
+	EnableCompression: true,
+	WriteBufferSize:   1024,
+	ReadBufferSize:    1024,
+}
+
 func Init(route string) {
 
 }
