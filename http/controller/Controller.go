d07c9e1df5cc0bf11cfad6ccace833567971d2f3
Timo <btimo8822@gmail.com>
2020-03-02 21:20:49 +0100

    add Authorized function

---
+++ http/controller/Controller.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"fmt"
+	"github.com/blobs-io/blobsgame/models/user"
+	"github.com/gofiber/fiber"
+	"strings"
+)
+
+func Authorized(ctx *fiber.Ctx) *user.User {
+	if ctx.Get("Authorization") == "" {
+		return nil
+	}
+
+	authArr := strings.Split(ctx.Get("Authorization"), " ")
+	if len(authArr) != 2 {
+		return nil
+	}
+
+	switch authArr[0] {
+	case "Session": // in case a session id was used to identify
+		dbUser, err := user.GetUser(authArr[1], user.UserSessionSearch)
+		if err != nil {
+			if err.Error() == user.UserNotFound {
+				return nil
+			} else {
+				fmt.Println(err)
+				return nil
+			}
+		}
+		return dbUser
+	default:
+		return nil
+	}
+}
