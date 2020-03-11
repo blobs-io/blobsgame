ef0484b83dce6a369c74c37c15936401eb041f9a
Timo <btimo8822@gmail.com>
2020-03-11 20:30:02 +0100

    add rest

---
+++ public/js/rest.js
@@ -0,0 +1,56 @@
+class RestClient {
+    constructor(key, authType, api) {
+        this.api = api || "/api/v1";
+        this._key = key;
+        this.authType = authType;
+    }
+
+    fetchUser(user) {
+        return fetch(`${this.api}/users/${user}`, {
+            headers: this.computedHeaders
+        });
+    }
+
+    fetchPromotions() {
+        return fetch(`${this.api}/promotions`);
+    }
+
+    fetchRooms() {
+        return fetch(`${this.api}/rooms`);
+    }
+
+    switchBlob(newBlob) {
+        return fetch(`${this.api}/blobs/switch`, {
+            method: "POST",
+            headers: this.computedHeaders,
+            body: JSON.stringify({ newBlob })
+        });
+    }
+
+    redeemDailyBonus() {
+        return fetch(`${this.api}/daily`, {
+            method: "POST",
+            headers: this.computedHeaders
+        });
+    }
+
+    ping() {
+        const before = Date.now();
+        return fetch(`${this.api}/ping`).then(() => Date.now() - before);
+    }
+
+    get key() {
+        return this.authType + " " + this._key;
+    }
+
+    get computedHeaders() {
+        return {
+            "Content-Type": "application/json",
+            Authorization: this.key
+        }
+    }
+
+    static extractSessionID() {
+        return document.cookie.split(/; */).find(v => v.startsWith("session=")).split("=")[1];
+    }
+}
